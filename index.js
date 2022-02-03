98a1a5e3e0b8627eec4e50d73d4a1d3dd90a5b27
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-02-03 15:42:47 -0800

    commit tree 72d5cd610a91eb3d218bd41df5e2999c0aba7962

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const scuberHq = 42;
+const oneBlockInFt = 264;
+
+function distanceFromHqInBlocks(pickupBlock){
+  return Math.abs(scuberHq - pickupBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock){
+  return distanceFromHqInBlocks(pickupBlock) * oneBlockInFt;
+}
+
+function distanceTravelledInFeet(start, destination){
+  return Math.abs(start - destination) * oneBlockInFt;
+}
+
+function calculatesFarePrice(start, destination){
+  const freeFt = 400;
+  const varRateFt = 2000;
+  const varRateMoney = 0.02;
+  const flatRateFt = 2500;
+  const flatRateMoney = 25;
+  const result = Math.abs(start - destination) * oneBlockInFt;
+
+  if (result <= freeFt){
+    return 0;
+  }
+
+  else if (result > freeFt && result <= varRateFt){
+    return (result - freeFt) * varRateMoney;
+  }
+
+  else if (result > varRateFt && result <= flatRateFt){
+    return flatRateMoney;
+  }
+
+  else if (result > flatRateFt){
+    return "cannot travel that far";
+  }
+}
